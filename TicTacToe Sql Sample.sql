ae6b4e66e29637e7583092b9c51f50bfcbc3eb1c
Marton Gerlei <marton.gerlei@fizzcode.hu>
2019-03-13 16:34:27 +0100

    no message

--- TicTacToe Sql Sample.sql
+++ TicTacToe Sql Sample.sql
@@ -80,12 +80,65 @@ EXEC upStep 1, 1, 'O'
 EXEC upStep 1, 2, 'O'
 EXEC upStep 1, 3, 'O'
 
+
 IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'upCheckWinning') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
 	DROP PROCEDURE upCheckWinning
 GO
 
 CREATE PROCEDURE upCheckWinning
+ @winningMark char(1) OUTPUT
 AS
 BEGIN
-	RETURN 1
+	SELECT @winningMark = b1.Mark FROM Board b1
+	INNER JOIN Board b2 ON b2.Mark = b1.Mark AND b2.X = b1.X AND b2.Y = b1.Y + 1
+	INNER JOIN Board b3 ON b3.Mark = b2.Mark AND b3.X = b2.X AND b3.Y = b2.Y + 1
 END
+
+-- Testing winning and notr winning cases
+TRUNCATE TABLE Board
+EXEC upStep 1, 1, 'O'
+EXEC upStep 1, 2, 'O'
+EXEC upStep 1, 3, 'O'
+
+DECLARE @testMark char(1)
+EXEC upCheckWinning @testMark OUTPUT
+
+IF @testMark <> 'O'
+	PRINT '!!! O should be the winner!'
+
+
+TRUNCATE TABLE Board
+EXEC upStep 1, 1, 'O'
+EXEC upStep 1, 2, 'O'
+EXEC upStep 1, 3, 'X'
+
+SET @testMark = NULL
+
+EXEC upCheckWinning @testMark OUTPUT
+
+IF @testMark IS NOT NULL
+	PRINT '!!! There shoud be no winner!'
+
+TRUNCATE TABLE Board
+EXEC upStep 1, 1, 'O'
+EXEC upStep 2, 1, 'O'
+EXEC upStep 3, 1, 'O'
+
+SET @testMark = NULL
+
+EXEC upCheckWinning @testMark OUTPUT
+
+IF @testMark <> 'O'
+	PRINT '!!! O should be the winner!'
+
+TRUNCATE TABLE Board
+EXEC upStep 1, 1, 'O'
+EXEC upStep 2, 2, 'O'
+EXEC upStep 3, 3, 'O'
+
+SET @testMark = NULL
+
+EXEC upCheckWinning @testMark OUTPUT
+
+IF @testMark <> 'O'
+	PRINT '!!! O should be the winner!'
