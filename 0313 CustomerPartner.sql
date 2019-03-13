ac68fa164a110452df313c135777581fb6644d35
Marton Gerlei <marton.gerlei@fizzcode.hu>
2019-03-13 12:18:35 +0100

    Initial commit.

---
+++ 0313 CustomerPartner.sql
@@ -0,0 +1,41 @@
+SELECT p.BusinessEntityID
+FROM Person.Person p
+WHERE p.BusinessEntityID NOT IN (
+	SELECT PersonID
+	FROM Sales.Customer
+	WHERE PersonID IS NOT NULL)
+ORDER BY p.BusinessEntityID ASC
+
+
+
+SELECT PersonID
+FROM Sales.Customer
+WHERE PersonID IS NOT NULL
+ORDER BY PersonID ASC
+
+SELECT DISTINCT PersonID
+FROM Sales.Customer
+WHERE PersonID IS NOT NULL
+ORDER BY PersonID ASC
+
+
+INSERT INTO Sales.Customer
+(PersonID, StoreID, TerritoryID, rowguid, ModifiedDate)
+SELECT TOP 1 PersonID, 934 AS StoreId, TerritoryID, NEWID() rowguid, GETDATE() ModifiedDate
+FROM Sales.Customer
+WHERE PersonID IS NOT NULL AND StoreID IS NOT NULL
+	AND StoreId <> 934
+-- 934
+
+
+SELECT LastName, MiddleName, FirstName
+FROM Person.Person
+
+SELECT LastName, MiddleName, FirstName
+FROM Person.Person
+ORDER BY LastName, MiddleName, FirstName
+
+
+/*SELECT *
+INTO Person.Person2
+FROM Person.Person*/
